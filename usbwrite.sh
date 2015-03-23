a9bd73f3319bbafeba45458d09f10fa9c72ca5e0
SlowRiot <slowriot@voxelstorm.com>
2015-03-23 18:18:18 +0000

    templated replacement system, pre-check

--- usbwrite.sh
+++ usbwrite.sh
@@ -7,15 +7,46 @@
 diskblacklistfile="disk_blacklist.txt"
 
 searchdevice="$1"
-sourcetree="$2"
+sourcedir="$2"
+templatedir="$3"
 mountpoint="./mnt"
 mkdir -p "$mountpoint"
 
-if [ "$searchdevice" = "" ] || [ "$sourcetree" = "" ]; then
-  echo "Usage: $0 targetdevice sourcetree"
-  echo "  i.e. $0 /dev/sdb ./tree/"
+if [ "$searchdevice" = "" ] || [ "$sourcedir" = "" ] || [ "$templatedir" = "" ]; then
+  echo "Usage: $0 targetdevice sourcedir templatedir"
+  echo "  i.e. $0 /dev/sdb ./source ./templates"
   exit 1
 fi
+
+echo "Verifying templated files in $templatedir..."
+pushd "$templatedir" >/dev/null
+IFS=$'\n'
+templatefilelist=$(find | grep ".template$")
+templatelistcount=$(echo "$templatefilelist" | wc -l)
+echo "  $templatelistcount templated files found."
+popd >/dev/null
+templateentrylist=$(
+  for templatefile in $templatefilelist; do
+    grep -o -P "\[\[.*?\]\]" "$templatedir/$templatefile"
+  done | sort | uniq | sed 's/^\[\[//;s/\]\]$//'
+)
+templateentrycount=$(echo "$templateentrylist" | wc -l)
+echo "  $templateentrycount unique template sources."
+for templateentry in $templateentrylist; do
+  templateentryfile="$templatedir/$templateentry"
+  echo "Checking $templateentryfile"
+  if ! [ -f "$templateentryfile" ]; then
+    referencedlocations=$(
+      for templatefile in $templatefilelist; do
+        fgrep -Hn "[[$templateentry]]" "$templatedir/$templatefile"
+      done
+    )
+    echo "ERROR: could not find template source file $templateentry"
+    echo "  referenced in:"
+    echo "$referencedlocations" | sed 's/^/    /;s/:/\n      line /;s/:/:\n        /'
+    exit 1
+  fi
+done
 
 count=0
 
@@ -54,18 +85,19 @@ while true; do
   rsync -a --info=progress2 "$tree" "$mountpoint"
 
   echo "Updating templated files..."
-  echo "$key_game" > "$mountpoint/Game Download Key.txt"
-  echo "$key_ost" > "$mountpoint/Soundtrack Download Key.txt"
+  
+  #echo "$key_game" > "$mountpoint/Game Download Key.txt"
+  #echo "$key_ost" > "$mountpoint/Soundtrack Download Key.txt"
 
   echo "Applying label..."
   # TODO
 
-  echo "Setting modified times to $(stat "$sourcetree" | grep ^Modify | cut -d ' ' -f 2-3)..."
+  echo "Setting modified times to $(stat "$sourcedir" | grep ^Modify | cut -d ' ' -f 2-3)..."
   OLDIFS="$IFS"
   IFS=$'\n'
   for file in $(find "$mountpoint"); do
     #echo "$file"; touch -c -t 201503211337.00 "$file"
-    echo "$file"; touch -c -r "$sourcetree" "$file"
+    echo "$file"; touch -c -r "$sourcedir" "$file"
   done
   IFS="$OLDIFS"
 
